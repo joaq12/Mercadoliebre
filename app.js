7a4daacd438e2280b2db290738042b3ee19c94b0
joaco <joaquin12_@hotmail.com>
2022-04-05 23:03:16 -0300

    agregando procfile

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
 
-app.listen(3030, () => {
+app.listen(process.env.PORT || 3030, () => {
   console.log("Servidor corriendo");
 });
