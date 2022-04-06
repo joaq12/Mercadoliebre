3befef6b2303abc47a3a10cd5fef1e109c08a234
joaco <joaquin12_@hotmail.com>
2022-04-05 22:23:57 -0300

    actualizando pag mercadoliebre

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
-/*const res = require("express/lib/response");*/
 const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
